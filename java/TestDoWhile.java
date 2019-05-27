1997ae0c5d330ea35a9a6ed54ec8ed43c1b1b910
fly <l>
2019-05-27 10:36:41 +0800

    同步

---
+++ java/TestDoWhile.java
@@ -0,0 +1,11 @@
+public class TestDoWhile {
+	public static void main(String args[]){
+		int x = 10;
+
+		do{
+			 System.out.print("value of x : " + x );
+			 x++;
+			 System.out.print("\n");
+		}while( x < 20 );
+ }
+}
