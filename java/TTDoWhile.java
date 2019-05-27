ed762a054a4b51630d8ee4b975a168c9f4dd4dd5
fly <l>
2019-05-27 10:24:55 +0800

    同步

---
+++ java/TTDoWhile.java
@@ -0,0 +1,11 @@
+public class TTDoWhile {
+	public static void main(String[] args) {
+		int x = 10;
+
+		do {
+			System.out.println("value of x:" + x);
+			x++;
+			System.out.println("\n");
+		} while (x < 20);
+	}
+}
